cd6476753c83e6fbe13b8097947fb0020e72d740
jianjiejin <jianjieking@163.com>
2018-12-12 14:34:42 +0800

    。。

--- src/main/java/TableFigureVisitor.java
+++ src/main/java/TableFigureVisitor.java
@@ -1,36 +1,26 @@
-
-import domain.TableFigureResult;
 import java.util.HashSet;
 import java.util.Set;
 
 public class TableFigureVisitor extends HplsqlBaseVisitor {
 
-    private String tableName = null;    //结果表名
-    private String procName = null;     //存储过程名
-    private String mergeTableName=null;     //merge表名
+    private String tableName = null;
+    private String procName = null;
 
-    private TableFigureResult tablere = new TableFigureResult();
     private Set<String> set = new HashSet<String>();
     private Set<String> sourLineSet = new HashSet<>();
+
 
     StringBuilder source = new StringBuilder();
     StringBuilder sb = new StringBuilder();
 
-
     /*
-        分析insert，获取目标表，循环调用visitFrom_table_name_clause，获取影响表，set用于影响表去重，sourLineSet用于画图去重
+        处理过程函数
      */
-    @Override
-    public Object visitInsert_stmt(HplsqlParser.Insert_stmtContext ctx) {
-        tableName = ctx.table_name().getText();
-        set.clear();
 
-        Object result = visitChildren(ctx);
-
-
+    private void main(){
         for (String str : set) {
             sb.append("\"" + str + "\"");
-            if (!set.isEmpty()) {
+            if (!set.isEmpty()  && !str.toUpperCase().contains("SESSION.")) {
                 sb.append(" -> " + "\"" + "存储过程：" + procName + "\"" + "\n");
                 if (!sourLineSet.contains(sb.toString())) {
                     source.append(sb.toString());
@@ -40,15 +30,30 @@ public class TableFigureVisitor extends HplsqlBaseVisitor {
             sb.delete(0, sb.length());
         }
 
-        sb.append("\"" + "存储过程：" + procName + "\"" + " -> " + "\"" + tableName + "\"" + "[color=red penwidth=2.0]" + "\n");
-        if (!sourLineSet.contains(sb.toString())) {
-            source.append(sb);
-            sourLineSet.add(sb.toString());
+        if(tableName!=null && !tableName.toUpperCase().contains("SESSION.") && !tableName.contains("ETL_ERRLOG_INFO")) {
+            sb.append("\"" + "存储过程：" + procName + "\"" + " -> " + "\"" + tableName + "\"" + "[color=red penwidth=2.0]" + "\n");
+            if (!sourLineSet.contains(sb.toString())) {
+                source.append(sb);
+                sourLineSet.add(sb.toString());
+            }
+            sb.delete(0, sb.length());
+            set.clear();
         }
-        sb.delete(0,sb.length());
+
+    }
 
 
+    /*
+        分析insert，获取目标表，循环调用visitFrom_table_name_clause，获取影响表，set用于影响表去重，sourLineSet用于画图去重
+     */
+    @Override
+    public Object visitInsert_stmt(HplsqlParser.Insert_stmtContext ctx) {
+
+        tableName = ctx.table_name().getText();
         set.clear();
+        Object result = visitChildren(ctx);
+        main();
+
         return result;
     }
 
@@ -60,7 +65,6 @@ public class TableFigureVisitor extends HplsqlBaseVisitor {
 
         String table_name = ctx.table_name().ident().getText();
         set.add(table_name);
-
         return visitChildren(ctx);
     }
 
@@ -80,19 +84,17 @@ public class TableFigureVisitor extends HplsqlBaseVisitor {
 
      */
 
-//    @Override
-//
-//    public Object visitMerge_stmt(HplsqlParser.Merge_stmtContext ctx) {
-//
-//        mergeTableName = ctx.merge_table(0).ident().getText();
-//        System.out.println(mergeTableName);
-//
-//        Object result = visitChildren(ctx);
-//
-//
-//        return result;
-//
-//    }
+    @Override
+
+    public Object visitMerge_stmt(HplsqlParser.Merge_stmtContext ctx) {
+
+        tableName = ctx.merge_table(0).table_name().ident().getText();
+        set.clear();
+        Object result = visitChildren(ctx);
+        main();
+        return result;
+
+    }
 
 
 
@@ -101,6 +103,5 @@ public class TableFigureVisitor extends HplsqlBaseVisitor {
     public String getSour() {
         return source.toString();
     }
-
 
 }
--- src/test/java/TableFigureTest.java
+++ src/test/java/TableFigureTest.java
@@ -1,4 +1,5 @@
 import domain.GraphViz;
+import domain.getFiles;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -7,32 +8,43 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class TableFigureTest {
 
     public static void main(String[] args) throws IOException {
-        String inputFile="src/test/java/TableFigureTest.txt";
-        if(args.length>0) inputFile = args[0];
-        InputStream is = System.in;
-        if(inputFile !=null)is= new FileInputStream(inputFile);
 
-
-        ANTLRInputStream input = new ANTLRInputStream(is);
-        HplsqlLexer lexer = new HplsqlLexer( input);
-        CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        HplsqlParser parser = new HplsqlParser(tokenStream);
-
-
-        /*
-            自定义visitor全部遍历
-         */
-        ParseTree tree = parser.program();
-        TableFigureVisitor visitor = new TableFigureVisitor();
-        visitor.visit(tree);
+        String fileDir =  "/Users/jianjie/Desktop/test/";
+        List<String> fileList = getFiles.getFileList(fileDir);
 
         GraphViz gv = new GraphViz();
         gv.addln(gv.start_graph());
-        gv.add(visitor.getSour());
+
+
+        for (int i = 0; i < fileList.size(); i++) {
+
+            String inputFile=fileList.get(i);
+            if(args.length>0) {
+                inputFile = args[0];
+            }
+
+            InputStream is = System.in;
+            if(inputFile !=null) {
+                is= new FileInputStream(inputFile);
+            }
+
+            ANTLRInputStream input = new ANTLRInputStream(is);
+            HplsqlLexer lexer = new HplsqlLexer( input);
+            CommonTokenStream tokenStream = new CommonTokenStream(lexer);
+            HplsqlParser parser = new HplsqlParser(tokenStream);
+
+            ParseTree tree = parser.program();
+            TableFigureVisitor visitor = new TableFigureVisitor();          //自定义visitor遍历
+            visitor.visit(tree);
+            gv.add(visitor.getSour());
+            System.out.println(visitor.getSour());
+        }
+
         gv.addln(gv.end_graph());
         String type = "pdf";
         gv.decreaseDpi();
@@ -40,7 +52,7 @@ public class TableFigureTest {
         String fileName="result";
         File out = new File(fileName+"."+ type);
         gv.writeGraphToFile( gv.getGraph( gv.getDotSource(), type ), out );
-        System.out.println(visitor.getSour());
+
 
 
     }
